e7960a8dcc3772145e603f137e4d5ae742e27900
priyankash6 <priyankasharma22098@gmail.com>
2022-05-29 15:59:01 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using UserListWithPagination.Models;
+
+namespace UserListWithPagination.Controllers
+{
+    public class UserController : Controller
+    {
+        Uri baseAddress = new Uri("https://reqres.in/api/users?page=2");
+        HttpClient client;
+        public UserController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+        }
+        public ActionResult Index()
+        {
+            APIData data =new APIData();
+            List<UserData> modelList = new List<UserData>();
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject<APIData>(responseBody);
+                modelList = data.data
+                    .OrderBy(u => u.first_name)
+                    .ToList();
+            }
+            return View(modelList);
+        }
+    }
+}
---
+++ Models/UserViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UserListWithPagination.Models
+{
+    public class APIData
+    {
+        public int page { get; set; }
+        public int per_page { get; set; }
+        public string total { get; set; }
+        public string total_pages { get; set; }
+        public List<UserData> data { get; set; }
+    }
+    public class UserData
+    {
+        public string id { get; set; }
+        [Display(Name = "First Name")]
+        public string first_name { get; set; }
+        [Display(Name = "Last Name")]
+        public string last_name { get; set; }
+        [Display(Name = "Email Address")]
+        public string email { get; set; }
+        [Display(Name = "Image")]
+       public string avatar { get; set; }
+    }
+}
